889eff800173cf91c87140089d91268b40e156e2
lazarlyutakov <lazarlyutakov@gmail.com>
2017-10-03 14:08:16 +0300

    add createUser function

--- SharedComuttingApp/app/src/main/java/com/example/lazarlyutakov/sharedcomuttingapp/authentication/register/RegisterActivity.java
+++ SharedComuttingApp/app/src/main/java/com/example/lazarlyutakov/sharedcomuttingapp/authentication/register/RegisterActivity.java
@@ -1,18 +1,33 @@
 package com.example.lazarlyutakov.sharedcomuttingapp.authentication.register;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.lazarlyutakov.sharedcomuttingapp.MainActivity;
 import com.example.lazarlyutakov.sharedcomuttingapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "EmailPassword";
+
     private Button regBtn;
     private String tbValue;
+    private EditText passwordBox;
+    private AutoCompleteTextView emailBox;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +36,11 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         regBtn = (Button)findViewById(R.id.btn_submit_registration);
         regBtn.setOnClickListener(this);
+
+        passwordBox = (EditText)findViewById(R.id.et_password_box);
+        emailBox = (AutoCompleteTextView)findViewById(R.id.et_email_box);
+
+        auth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -32,11 +52,68 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     }
 
     @Override
-    public void onClick(View view) {
-        AutoCompleteTextView username = (AutoCompleteTextView)findViewById(R.id.et_username_box);
-        tbValue = username.getText().toString();
-        Toast.makeText(this, tbValue + "", Toast.LENGTH_SHORT).show();
-        System.out.println("HERE " + tbValue);
-
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
     }
+
+    @Override
+    public void onClick(View view) {
+        createAccount(emailBox.getText().toString(), passwordBox.getText().toString());
+        Toast.makeText(this, emailBox.getText().toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "gotovo", Toast.LENGTH_SHORT).show();
+    }
+
+    private void createAccount(String email, String password) {
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                       // hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void updateUI(FirebaseUser user) {
+       // hideProgressDialog();
+        if (user != null) {
+            /*mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), user.isEmailVerified()));
+            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));*/
+
+            /*findViewById(R.id.email_password_buttons).setVisibility(View.GONE);
+            findViewById(R.id.email_password_fields).setVisibility(View.GONE);
+            findViewById(R.id.signed_in_buttons).setVisibility(View.VISIBLE);
+
+            findViewById(R.id.verify_email_button).setEnabled(!user.isEmailVerified());*/
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else {
+            /*mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);*/
+
+            /*findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
+            findViewById(R.id.email_password_fields).setVisibility(View.VISIBLE);
+            findViewById(R.id.signed_in_buttons).setVisibility(View.GONE);*/
+        }
+    }
+
 }

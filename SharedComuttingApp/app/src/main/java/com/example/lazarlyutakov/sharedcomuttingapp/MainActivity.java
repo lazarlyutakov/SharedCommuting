732b379af86b9531cc4e415512d8fc37e0c7452e
lazarlyutakov <lazarlyutakov@gmail.com>
2017-10-11 13:42:20 +0300

    when starting the app, navigate to LoggedInActivity, if logged user exists

--- SharedComuttingApp/app/src/main/java/com/example/lazarlyutakov/sharedcomuttingapp/MainActivity.java
+++ SharedComuttingApp/app/src/main/java/com/example/lazarlyutakov/sharedcomuttingapp/MainActivity.java
@@ -5,25 +5,37 @@ import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.lazarlyutakov.sharedcomuttingapp.authentication.loggedIn.LoggedInActivity;
 import com.example.lazarlyutakov.sharedcomuttingapp.fragments.ButtonsFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import mehdi.sakout.fancybuttons.FancyButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private Fragment btnsFragment;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser loggedUser = auth.getCurrentUser();
+
         btnsFragment = new ButtonsFragment();
 
-        getFragmentManager()
-                .beginTransaction()
-                .replace(R.id.btns_fragment, btnsFragment)
-                .commit();
-
+        if(loggedUser != null){
+            Intent intent = new Intent(this, LoggedInActivity.class);
+            startActivity(intent);
+        }
+        else{
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.btns_fragment, btnsFragment)
+                    .commit();
+        }
     }
 }
